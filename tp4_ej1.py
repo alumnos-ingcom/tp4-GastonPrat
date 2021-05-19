05d0eb2f14790e762e74e0b6e013f5d3e5efd217
Gaston Prat <gastonemanuelprat@gmail.com>
2021-05-19 12:09:26 -0300

    Finalizando ejercicio 1

--- tp4_ej1.py
+++ tp4_ej1.py
@@ -20,19 +20,19 @@ def ingreso_entero_reintento(mensaje, cantidad_reintentos=5):
     while cantidad_reintentos > 0:
         try:
             return ingreso_entero(mensaje)
-        except IngresoIncorrecto:
-            cantidad_reintentos = cantidad_reintentos - 1
+        except IngresoIncorrecto as err:
             print(f"No ingresaste un numero entero, te quedan {cantidad_reintentos} intentos")
+            cantidad_reintentos = cantidad_reintentos - 1
     raise IngresoIncorrecto("Te quedaste sin mas intentos")
 
 def ingreso_entero_restringido(mensaje,minimo = 0, maximo = 10):
     mensaje2 = f"Ingresar un numero entre {minimo} y {maximo}"
     num = ingreso_entero(mensaje2)
-    if (num >= 0 and num <= 10):
+    if (num >= minimo and num <= maximo):
         return num
         print(f"El numero ingresado {num} es correcto")
     else:
-        print("El nomero ingresado no pertenece al rango entre 0 y 10")
+        raise IngresoIncorrecto("El nomero ingresado no pertenece al rango entre 0 y 10")
     
     
 def prueba():
