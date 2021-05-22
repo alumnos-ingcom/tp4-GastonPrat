b44b952a4ce3fd127eaa5493af0a06292623636f
Gaston Prat <gastonemanuelprat@gmail.com>
2021-05-22 20:16:53 -0300

    quitando el method sort

--- tp4_ej8.py
+++ tp4_ej8.py
@@ -7,20 +7,63 @@ from tp4_ej1 import ingreso_entero, IngresoIncorrecto
 
 def ordenar_mayor_a_menor(uno, dos, tres):
     mayor_menor = []
-    uno = mayor_menor.append(ingreso_entero(uno))
-    dos = mayor_menor.append(ingreso_entero(dos))
-    tres = mayor_menor.append(ingreso_entero(tres))
-    mayor_menor.sort(key=None, reverse=True)
+    uno = ingreso_entero(uno)
+    dos = ingreso_entero(dos)
+    tres = ingreso_entero(tres)
+    if uno >= dos:
+        if uno >= tres:
+            mayor_menor.append(uno)
+            if dos >= tres:
+                mayor_menor.append(dos)
+                mayor_menor.append(tres)
+            else:
+                mayor_menor.append(tres)
+                mayor_menor.append(dos)
+        else:
+            mayor_menor.append(tres)
+            mayor_menor.append(uno)
+            mayor_menor.append(dos)
+    else:
+        if dos >= tres:
+            mayor_menor.append(dos)
+            if uno >= tres:
+                mayor_menor.append(uno)
+                mayor_menor.append(tres)
+        else:
+            mayor_menor.append(tres)
+            mayor_menor.append(dos)
+            mayor_menor.append(uno)
     return tuple(mayor_menor)
 
 def ordenar_menor_a_mayor(uno, dos, tres):
     menor_mayor = []
-    uno = menor_mayor.append(ingreso_entero(uno))
-    dos = menor_mayor.append(ingreso_entero(dos))
-    tres = menor_mayor.append(ingreso_entero(tres))
-    menor_mayor.sort(key=None, reverse=False)
+    uno = ingreso_entero(uno)
+    dos = ingreso_entero(dos)
+    tres = ingreso_entero(tres)
+    if uno <= dos:
+        if uno <= tres:
+            menor_mayor.append(uno)
+            if dos <= tres:
+                menor_mayor.append(dos)
+                menor_mayor.append(tres)
+            else:
+                menor_mayor.append(tres)
+                menor_mayor.append(dos)
+        else:
+            menor_mayor.append(tres)
+            menor_mayor.append(uno)
+            menor_mayor.append(dos)
+    else:
+        if dos <= tres:
+            menor_mayor.append(dos)
+            if uno <= tres:
+                menor_mayor.append(uno)
+                menor_mayor.append(tres)
+        else:
+            menor_mayor.append(tres)
+            menor_mayor.append(dos)
+            menor_mayor.append(uno)
     return tuple(menor_mayor)
-
 def prueba():
     print("El siguiente programa imprimirá los numero de Mayor a Menor")
     print(ordenar_mayor_a_menor("Ingresa un Número", "Ingresa un otro "
