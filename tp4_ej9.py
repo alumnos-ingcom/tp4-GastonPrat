c3baf0bac5bdbd56345c9dee6350ef905aa9996d
Gaston Prat <gastonemanuelprat@gmail.com>
2021-05-23 18:58:37 -0300

    corrigiendo algunas cosas

--- tp4_ej9.py
+++ tp4_ej9.py
@@ -6,7 +6,6 @@
 from tp4_ej1 import ingreso_entero, IngresoIncorrecto
 
 def es_primo(numero_in):
-    numero_in = ingreso_entero(numero_in)
     mitad = int(numero_in / 2)
     resto = True
     while mitad > 1:
@@ -19,7 +18,12 @@ def es_primo(numero_in):
 def prueba():
     print("Este programa le indicara si el numero ingresado es "
           "un Número Primo")
-    print(es_primo("Ingrese un Número Entero"))
+    numero_in = ingreso_entero("Ingrese un Número Entero")
+    resto = es_primo(numero_in)
+    if resto == True:
+        print(f"{numero_in} es un número primo")
+    else:
+        print(f"{numero_in} no es primo")
     
 if __name__ == "__main__":
     prueba()
