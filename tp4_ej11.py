897a44f4152c3d16818f1bc6e44b7db9a935c139
Gaston Prat <gastonemanuelprat@gmail.com>
2021-05-23 18:58:15 -0300

    corrigiendo algunas cosas

--- tp4_ej11.py
+++ tp4_ej11.py
@@ -9,13 +9,17 @@ def es_palindromo(texto):
     texto = texto.lower()
     texto = texto.replace(" ", "")
     inversa = texto[ : : -1]
-    
-    print(texto == inversa)
-        
+    palindromo = inversa == texto
+    return palindromo    
     
 def prueba():
-    print("Se indicara si una palabra o frase es un Palindromo")
-    es_palindromo(input("Ingrese una palabra o frase: "))
+    texto = input("Ingrese una frase o palabra: ")
+    palindromo = es_palindromo(texto)
+    if palindromo == True:
+        print(f"la palabra o frase {texto} es un Palíndromo")
+    else:
+        print(f"{texto} no es un Palíndromo")
+
     
 if __name__ == "__main__":
     prueba()
