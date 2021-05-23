329695b8625f43dcb6639703e935c871a1b71ac6
Gaston Prat <gastonemanuelprat@gmail.com>
2021-05-23 18:58:27 -0300

    corrigiendo algunas cosas

--- tp4_ej10.py
+++ tp4_ej10.py
@@ -6,17 +6,19 @@
 from tp4_ej1 import ingreso_entero, IngresoIncorrecto
 
 def factores_primos(numero):
-    numero = ingreso_entero(numero)
     primos = []
     for i in range(2, numero+1):
         while numero % i == 0:
             primos.append(i)
             numero = numero / i
-    return tuple(primos)
+    primos = tuple(primos)
+    return primos
 
 def prueba():
     print("El siguiente programa arroja los factores primos")
-    print(factores_primos("Ingrese un Número Entero"))
+    numero = ingreso_entero("Ingrese un Número entero")
+    primos = factores_primos(numero)
+    print(f"los Factores primos de {numero} son {primos}")
     
 if __name__ == "__main__":
     prueba()
